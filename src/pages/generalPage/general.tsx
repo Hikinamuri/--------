9910d8752057c668c5adeae1ee9c3177622b8c02
Hikinamuri <Hikinamuri@mail.ru>
2024-07-23 16:51:52 +0300

    add: added third content block

--- src/pages/generalPage/general.tsx
+++ src/pages/generalPage/general.tsx
@@ -4,7 +4,7 @@ import waiting from '../../assets/img/howItsWork/waiting.svg';
 import delivery from '../../assets/img/howItsWork/delivery-truck.svg';
 import secure from '../../assets/img/howItsWork/secure.svg';
 import money from '../../assets/img/howItsWork/money-bags.svg';
-
+import man from '../../assets/img/man.png';
 
 
 const styles = require('./style.module.css');
@@ -35,9 +35,9 @@ export const GeneralPage = () => {
                 </div>
             </div>
             <div className={styles.content}>
-                <div className={styles.content__how_its_work}>
+                <div className={styles.content__first_block}>
                     <h1>Как это работает</h1>
-                    <div className={styles.content__how_its_work__images}>
+                    <div className={styles.content__first_block__images}>
                         <div>
                             <img src={waiting} alt="waiting" />
                             <p>Прочитайте задание внимательно</p>
@@ -59,6 +59,17 @@ export const GeneralPage = () => {
                             <span>Ну тут все просто, не я придумал правила, но думаю так и будет)))</span>
                         </div>
                     </div>
+                </div>
+                <div className={styles.content__second_block}>
+                    <div className={styles.content__second_block__info}> 
+                        <h1>Круто, ты дошел до третьего блока</h1>
+                        <span>
+                            63% опрошенных пользовались кредитами из-за того, что не могли покрыть непредвиденные расходы свыше 15 000 ₽.
+                            <pre></pre>
+                            Доступ к заработанным деньгам помогает отказаться от кредитов и экономить деньги на процентах и штрафах.
+                        </span>
+                    </div>
+                    <img src={man} alt="Мужчина" />
                 </div>
             </div>
         </>
